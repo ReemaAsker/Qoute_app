e824d3702daf7039a57cf59e9aa01b843b867aee
Reema Asker <reema1822000@gmail.com>
2023-07-23 23:26:04 +0300

    refactor UI to be more beautiful for quote

--- lib/View/pages/HomePage.dart
+++ lib/View/pages/HomePage.dart
@@ -50,34 +50,43 @@ class _HomePageState extends State<HomePage> {
             filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
             child: Stack(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Icon(
-                          Icons.refresh,
-                          size: 34,
-                          color: Colors.black,
+                Padding(
+                  padding: const EdgeInsets.only(right: 30.0, top: 30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.white.withOpacity(0.2)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Icon(
+                              Icons.refresh,
+                              size: 34,
+                              color: Colors.black,
+                            ),
+                          ),
                         ),
-                      ),
-                      onPressed: () {
-                        widget.qoute.getRandomQoute().whenComplete(() {
-                          RandomImage(imgDesc: widget.qoute.desc)
-                              .getRandomImag()
-                              .then((value) {
-                            Navigator.pushReplacement(context,
-                                MaterialPageRoute(builder: (context) {
-                              return HomePage(qoute: widget.qoute, img: value);
-                            }));
+                        onPressed: () {
+                          widget.qoute.getRandomQoute().whenComplete(() {
+                            RandomImage(imgDesc: widget.qoute.desc)
+                                .getRandomImag()
+                                .then((value) {
+                              Navigator.pushReplacement(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return HomePage(
+                                    qoute: widget.qoute, img: value);
+                              }));
+                            });
                           });
-                        });
 
-                        // getDataFromit();
-                      },
-                    ),
-                  ],
+                          // getDataFromit();
+                        },
+                      ),
+                    ],
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(
@@ -86,53 +95,70 @@ class _HomePageState extends State<HomePage> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        // Padding(
+                        //   padding: const EdgeInsets.only(left: 300.0, top: 20),
+                        //   child: Image(
+                        //     image: AssetImage('assets/images/right-quotes.png'),
+                        //     height: 24,
+                        //     width: 24,
+                        //     color: Colors.black,
+                        //   ),
+                        // ),
+
                         Padding(
-                          padding: const EdgeInsets.only(left: 300.0, top: 20),
-                          child: Image(
-                            image: AssetImage('assets/images/right-quotes.png'),
-                            height: 24,
-                            width: 24,
-                            color: Colors.black,
+                          padding: const EdgeInsets.only(top: 70.0),
+                          child: Stack(
+                            children: [
+                              // Row(
+                              //   mainAxisAlignment: MainAxisAlignment.end,
+                              //   children: [
+                              //     Image(
+                              //       image: AssetImage(
+                              //           'assets/images/right-quotes.png'),
+                              //       height: 50,
+                              //       width: 50,
+                              //       color: Colors.black,
+                              //     ),
+                              //   ],
+                              // ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  RotationTransition(
+                                    turns:
+                                        new AlwaysStoppedAnimation(180 / 360),
+                                    child: Image(
+                                      image: AssetImage(
+                                          'assets/images/right-quotes.png'),
+                                      height: 80,
+                                      width: 80,
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(45),
+                                // color: Colors.white10,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.white30,
+                                ),
+                                child: Text(
+                                  textAlign: TextAlign.center,
+                                  widget.qoute.content,
+                                  style: TextStyle(
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 2,
+                                      fontFamily: 'EduSABeginner'),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                         SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(30),
-                          // color: Colors.white10,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.white30,
-                          ),
-                          child: Text(
-                            textAlign: TextAlign.center,
-                            widget.qoute.content,
-                            style: TextStyle(
-                                fontSize: 30.0,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 2,
-                                fontFamily: 'EduSABeginner'),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        RotationTransition(
-                          turns: new AlwaysStoppedAnimation(180 / 360),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 320.0),
-                            child: Image(
-                              image:
-                                  AssetImage('assets/images/right-quotes.png'),
-                              height: 24,
-                              width: 24,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 100,
+                          height: 50,
                         ),
                         Container(
                           padding: EdgeInsets.all(10),
